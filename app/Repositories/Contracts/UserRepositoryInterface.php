84c4749bca6408101a491e7699063c1c0cc8c9e6
Reyhan Amando <reyhan.amando35@gmail.com>
2026-01-09 13:17:38 +0700

    created contracts

---
+++ app/Repositories/Contracts/UserRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\User;
+
+interface UserRepositoryInterface
+{
+    public function getAll();
+
+    public function findById($id);
+
+    public function findByUsername(string $username): ?User;
+
+    public function store(array $data);
+
+    public function update($id, array $data);
+
+    public function delete($id);
+}
